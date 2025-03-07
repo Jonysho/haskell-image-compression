0c71d61c0b6397f3aa8b4c1af16e3394fd8bf6d2
jonysho <jonathanysho@gmail.com>
2025-03-07 21:29:39 +0000

    reset

--- Main.hs
+++
@@ -1,8 +0,0 @@
-module Main where
-
-import ImageCompression (compressImage)
-
-main :: IO ()
-main = do
-    -- Replace "uom.png" and "output.png" with your actual input and output file paths
-    compressImage "uom.png" "output.png"
